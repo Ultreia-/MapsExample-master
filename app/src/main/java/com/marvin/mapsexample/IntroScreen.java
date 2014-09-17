75d171367278b0e0b7204c990a0322f24f1b6eab
Ultreia- <201304196@post.au.dk>
2014-09-17 12:24:06 +0200

    First commit
    ja

---
+++ app/src/main/java/com/marvin/mapsexample/IntroScreen.java
@@ -0,0 +1,49 @@
+package com.marvin.mapsexample;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by rasmus on 9/16/14.
+ */
+public class IntroScreen extends FragmentActivity {
+
+    Button newGame;
+    Button joinGame;
+    EditText inputField;
+    static String username = "";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+        //prepare dem buttons
+        newGame = (Button) findViewById(R.id.new_game);
+        joinGame = (Button) findViewById(R.id.join_game);
+        inputField = (EditText)findViewById(R.id.edittext);
+
+        newGame.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                username = inputField.getText().toString();
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        joinGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                username= inputField.getText().toString();
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/marvin/mapsexample/MapsActivity.java
@@ -0,0 +1,76 @@
+package com.marvin.mapsexample;
+
+import android.app.Dialog;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapsActivity extends FragmentActivity implements LocationListener {
+
+    GoogleMap googleMap;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+
+        if(status != ConnectionResult.SUCCESS){
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+        } else {
+            TextView tvLocation = (TextView) findViewById(R.id.tv_location);
+            SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+            googleMap = fm.getMap();
+            googleMap.setMyLocationEnabled(true);
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String provider = locationManager.getBestProvider(criteria, true);
+            Location location = locationManager.getLastKnownLocation(provider);
+            tvLocation.setText("Welcome, " + IntroScreen.username);
+
+
+            if(location != null) {
+                onLocationChanged(location);
+            }
+            locationManager.requestLocationUpdates(provider, 20000, 0, this);
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
