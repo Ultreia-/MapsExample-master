9ad02fb4e9d7fd9853a53a4b2a93ecf5cf127c47
Christian Helding <helding13@gmail.com>
2014-09-24 18:34:40 +0200

    Added functions activity
    U know

---
+++ app/src/main/java/com/marvin/mapsexample/FunctionScreen.java
@@ -0,0 +1,44 @@
+package com.marvin.mapsexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by christianheldingsrensen on 24/09/14.
+ */
+public class FunctionScreen extends FragmentActivity {
+
+    Button loadTest;
+    Button mapTest;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.func_screen);
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        loadTest = (Button) findViewById(R.id.loading_test);
+        mapTest = (Button) findViewById(R.id.map_test);
+
+        text.setText("Welcome, " + IntroScreen.username);
+
+        loadTest.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LoadingScreen.class);
+                startActivity(i);
+            }
+        });
+
+        mapTest.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
--- app/src/main/java/com/marvin/mapsexample/IntroScreen.java
+++ app/src/main/java/com/marvin/mapsexample/IntroScreen.java
@@ -32,7 +32,7 @@ public class IntroScreen extends FragmentActivity {
         newGame.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 username = inputField.getText().toString();
-                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                Intent i = new Intent(getApplicationContext(), FunctionScreen.class);
                 startActivity(i);
             }
         });
