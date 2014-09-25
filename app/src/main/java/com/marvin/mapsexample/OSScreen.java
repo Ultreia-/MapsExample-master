ccebc02a3333a483dcc081fc70428ec4f4806428
Christian Helding <helding13@gmail.com>
2014-09-25 17:50:46 +0200

    Added tabs to OSScreen
    Uggoly as fuck tho

--- app/src/main/java/com/marvin/mapsexample/OSScreen.java
+++ app/src/main/java/com/marvin/mapsexample/OSScreen.java
@@ -2,6 +2,7 @@ package com.marvin.mapsexample;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 /**
@@ -16,6 +17,23 @@ public class OSScreen extends FragmentActivity{
         TextView text = (TextView) findViewById(R.id.textView);
         text.setText("Agent: " + IntroScreen.username);
 
+        TabHost th = (TabHost) findViewById(R.id.tabHost);
+        th.setup();
+
+        TabHost.TabSpec specs = th.newTabSpec("tag1");
+        specs.setContent(R.id.tab1);
+        specs.setIndicator("Mission Log");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("tag2");
+        specs.setContent(R.id.tab2);
+        specs.setIndicator("Notes");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("tag3");
+        specs.setContent(R.id.tab3);
+        specs.setIndicator("Progress");
+        th.addTab(specs);
 
     }
 }
