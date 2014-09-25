63f8b0ba7a1f3eac208b117ad0f28654ad7ac4a8
Christian Helding <helding13@gmail.com>
2014-09-25 20:25:05 +0200

    Update time changed

--- app/src/main/java/com/marvin/mapsexample/MapsActivity.java
+++ app/src/main/java/com/marvin/mapsexample/MapsActivity.java
@@ -47,7 +47,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
             if(location != null) {
                 onLocationChanged(location);
             }
-            locationManager.requestLocationUpdates(provider, 3000, 0, this);
+            locationManager.requestLocationUpdates(provider, 10000, 0, this);
         }
 
         addTestMarkerToMap();
