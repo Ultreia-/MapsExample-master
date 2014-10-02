0ccf1d6b02c74c9f66833e7336a4fbb97b53df6d
nicklasjust <nicklasjust@Nicklass-MacBook-Pro.local>
2014-10-02 14:25:09 +0200

    First time?!?!
    Checks if if is the first time the user is using the app

--- app/src/main/java/com/marvin/mapsexample/OSScreen.java
+++ app/src/main/java/com/marvin/mapsexample/OSScreen.java
@@ -1,5 +1,9 @@
 package com.marvin.mapsexample;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TabHost;
@@ -10,9 +14,41 @@ import android.widget.TextView;
  */
 public class OSScreen extends FragmentActivity{
 
+    public static final String firstTime = "firstTime";
+    public static final String myPreferences = "MyPrefs" ;
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.os_screen);
+
+        pref = getSharedPreferences(myPreferences, Context.MODE_PRIVATE);
+        editor = pref.edit();
+
+        if(!pref.contains(firstTime)) {
+            editor.putBoolean(firstTime, true);
+            editor.commit();
+            new AlertDialog.Builder(this)
+                    .setTitle("Halp meeeee!!!")
+                    .setMessage("Hola bromigos! I could really use some help from you, so if you want to, suck ma penis!")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            editor.putBoolean(firstTime, false);
+                            editor.commit();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            editor.putBoolean(firstTime, false);
+                            editor.commit();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
 
         TextView text = (TextView) findViewById(R.id.textView);
         text.setText("Agent: " + IntroScreen.username);
