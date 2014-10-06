47252eb7e1b17c095cfcb247a82bb8d25052d27c
TobiasEmilHarbo <tobiasharbo@gmail.com>
2014-10-06 14:58:03 +0200

    Restrestrestrest

--- app/src/main/java/com/marvin/mapsexample/GameStartScreen.java
+++ app/src/main/java/com/marvin/mapsexample/GameStartScreen.java
@@ -1,7 +1,27 @@
 package com.marvin.mapsexample;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.marvin.mapsexample.HelperPackage.Game;
+import com.marvin.mapsexample.HelperPackage.RestServer;
+
 /**
  * Created by Tobias on 05-10-2014.
  */
-public class GameStartScreen {
+public class GameStartScreen extends RestServer {
+
+    TextView partnerName;
+    TextView playerName;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_start_screen);
+
+        playerName = (TextView) findViewById(R.id.playerName);
+        partnerName = (TextView) findViewById(R.id.partnerName);
+
+        playerName.setText("Welcome, " + Game.player.getName());
+        partnerName.setText("Welcome, " + Game.partner.getName());
+    }
 }
--- app/src/main/java/com/marvin/mapsexample/WaitForPartnerScreen.java
+++ app/src/main/java/com/marvin/mapsexample/WaitForPartnerScreen.java
@@ -2,16 +2,21 @@ package com.marvin.mapsexample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.marvin.mapsexample.HelperPackage.Game;
+import com.marvin.mapsexample.HelperPackage.Player;
 import com.marvin.mapsexample.HelperPackage.RestServer;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Tobias on 03-10-2014.
@@ -19,37 +24,59 @@ import java.util.HashMap;
 public class WaitForPartnerScreen extends RestServer {
 
     Button cancelNewGameButton;
+    private TimerTask timerTask;
+    private Timer timer;
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.wait_for_partner_screen);
 
-        Toast.makeText(getBaseContext(), "Looking for your partner...", Toast.LENGTH_SHORT).show();
-
         cancelNewGameButton = (Button) findViewById(R.id.cancelGame);
+
+        final Toast toast = Toast.makeText(getBaseContext(), "Looking for a partner...", Toast.LENGTH_SHORT);
+        //toast.show();
 
         cancelNewGameButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
 
-               requestPost("http://marvin.idyia.dk/game/cancel",
-                    new HashMap<String, String>() {{
-                        put("gameId", Game.id);
-                    }},
-                    new CancelGameCallback());
-            }
-        });
-    }
-
-    /*
-    @Override
-    public void onBackPressed() {
-        requestPost("http://marvin.idyia.dk/game/cancel",
+           requestPost("http://marvin.idyia.dk/game/cancel",
                 new HashMap<String, String>() {{
                     put("gameId", Game.id);
                 }},
                 new CancelGameCallback());
-    }*/
+            }
+        });
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (Looper.myLooper() == null) {
+                    Looper.prepare();
+                }
+
+                toast.show();
+
+                requestPost("http://marvin.idyia.dk/game/hasPartnerJoined",
+                    new HashMap<String, String>() {{
+                        put("gameId", Game.id);
+                    }},
+                    new FindPartnerCallback());
+            }
+        };
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, 4000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getApplicationContext(), IntroScreen.class);
+        startActivity(i);
+
+        timerTask.cancel();
+        timer.cancel();
+    }
 
     private class CancelGameCallback implements RestCallbackInterface {
 
@@ -58,6 +85,41 @@ public class WaitForPartnerScreen extends RestServer {
             Game.id = null;
             Intent i = new Intent(getApplicationContext(), IntroScreen.class);
             startActivity(i);
+        }
+    }
+
+    private class FindPartnerCallback implements RestCallbackInterface {
+
+        public void onEndRequest(JSONObject result)
+        {
+            try {
+
+                String status = result.getString("status");
+
+                if(status.equals("200"))
+                {
+                    JSONObject data = result.getJSONObject("data");
+                    Boolean partnerHasJoined = data.getBoolean("partnerHasJoined");
+
+                    if(partnerHasJoined)
+                    {
+                        String partnerId = data.getString("partnerId");
+                        String partnerName = data.getString("partnerName");
+                        Game.player = new Player(partnerId, partnerName);
+
+                        Intent i = new Intent(getApplicationContext(), GameStartScreen.class);
+                        startActivity(i);
+                    }
+                }
+                else throw new Exception(status);
+
+            } catch (JSONException e) {
+                //e.printStackTrace();
+                Toast.makeText(getBaseContext(), "Could not create player; status 500", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                //e.printStackTrace();
+                Toast.makeText(getBaseContext(), "Could not create player; status "+e, Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
