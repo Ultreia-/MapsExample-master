e619cd3ee796f9eff61e369bbde6b16a2d2556f3
Ultreia- <201304196@post.au.dk>
2014-10-06 15:11:28 +0200

    Startscreen with startactivity

--- app/src/main/java/com/marvin/mapsexample/GameStartScreen.java
+++ app/src/main/java/com/marvin/mapsexample/GameStartScreen.java
@@ -1,5 +1,8 @@
 package com.marvin.mapsexample;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import com.marvin.mapsexample.HelperPackage.RestServer;
 
@@ -10,6 +13,18 @@ public class GameStartScreen extends RestServer {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_start_screen);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Look at this dialog!")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                        startActivity(i);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
 
     }
 }
